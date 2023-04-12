cd511bd5f4be750ac0c07c3ea92fde6e3139e8b1
Ammar Haziq <108142876+marziq@users.noreply.github.com>
2023-04-12 10:20:43 +0800

    Update main.java

--- main.java
+++ main.java
@@ -14,22 +14,25 @@ public class main {
         java.util.Scanner input = new java.util.Scanner(System.in); // to read other input
         Customer[] customer = new Customer[100];
         Product[] product = new Product[5];
-        Booking order = new Booking();
+        Booking booking = new Booking();
         Agent[] agent = new Agent[3];
 
         for(int i = 0;i < customer.length; i++)
             customer[i] = new Customer();
-        int customerCounts = 0;
+        int customerCounts = 2; // start 2 because already have 3 customer (2 is index)
 
-        agent[0] = new Agent("Ali Andalas", "011-113243213", "KL", 0);
-        agent[1] = new Agent("George Lee", "012-343213377", "SEREMBAN", 0);
-        agent[2] = new Agent("Maryam Abdullah", "018-57715660", "JOHOR", 0);
+        customer[0] = new Customer("Ammar Haziq", "0198389538", "Melayu Raya");
+        customer[1] = new Customer("Ahmad Haikal", "0111234765", "Pekan Nanas");
+        customer[2] = new Customer("Mior Ahmad", "01234567890", "Pontian");
+        agent[0] = new Agent("George Lopez", "011-113243213", "KL", 0);
+        agent[1] = new Agent("Giga Hadid", "012-343213377", "SEREMBAN", 0);
+        agent[2] = new Agent("Mohd Salahuddin", "018-57715660", "JOHOR", 0);
         product[0] = new Product(1,"Langkawi Island Tour", 500);
         product[1] = new Product(2,"Krabi Island Tour", 800);
         product[2] = new Product(3,"Perhentian Island Tour", 700);
 
         do{
-            System.out.println("\nWelcome to Skidamark Travel Agency\n\nChoose agent: ");
+            System.out.println("\nWelcome to Dreamy Flight booking\n\nChoose agent: ");
             for(int i = 0; i < agent.length; i++)
                 System.out.println("   "+ (i+1) + ". " + agent[i].getName());
 
@@ -37,15 +40,23 @@ public class main {
             chooseAgent = input.nextInt();
             chooseAgent--;
             System.out.print("Enter Customer Name: ");
-            String customerName = reader.readLine();
-
+            booking.setCustomerName(reader.readLine());
+            String customerName = booking.getCustomerName();
             for(int i = 0; i <= customerCounts; i++)
             if(customerName.equalsIgnoreCase(customer[i].getName())){
                 isExist = true;
                 break;
             }
-            if(isExist)
-                System.out.println("Customer is a registered customer");
+            if(isExist){
+                System.out.println("Customer is a registered customer\n");
+                for(int i = 0; i <= customerCounts; i++)
+                    if(customerName.equalsIgnoreCase(customer[i].getName())){
+                        System.out.println("Name: " + customer[i].getName());
+                        System.out.println("Contact no: " + customer[i].getContact());
+                        System.out.println("Address: " + customer[i].getAddress());
+                        break;
+                    }
+            }
             else{
                 System.out.println("Customer is not yet registered\n");
                 System.out.print("Enter Name: ");
@@ -54,15 +65,15 @@ public class main {
                 String customerContact = reader.readLine();
                 System.out.print("Enter Address: ");
                 String customerAddress = reader.readLine();
-                System.out.println("New Customer succesfully added\n");
+                System.out.println("New Customer successfully added\n");
                 customer[customerCounts] = new Customer(customerName,customerContact,customerAddress);
                 customerCounts++;
             }
 
             System.out.print("Make new booking?(Y/N): ");
-            char booking = input.next().charAt(0);
+            char bookingChoice = input.next().charAt(0);
 
-            switch(booking){
+            switch(bookingChoice){
                 case 'y': case 'Y':break;
                 default: continue;
             }
@@ -72,14 +83,14 @@ public class main {
             for(int i = 0;i < Product.getProductCounts(); i++)
                 System.out.println("  " + (i+1) + ". " + product[i].getName() + "    RM" + product[i].getPrice());
             System.out.print("Choose package: ");
-            int customerPackage = input.nextInt();
-            customerPackage--;
+            booking.setProductCode(input.nextInt());
+            int customerPackage = booking.getProductCode() - 1;
             System.out.print("Enter Quantity: ");
-            int quantity = input.nextInt();
+            booking.setQuantity(input.nextInt());
 
-            double total = quantity*product[customerPackage].getPrice(); // total amount
+            double total = booking.getQuantity()*product[customerPackage].getPrice(); // total amount
 
-            System.out.println("You have chosen " + product[customerPackage].getName() + " for " + quantity + " pax");
+            System.out.println("You have chosen " + product[customerPackage].getName() + " for " + booking.getQuantity() + " pax");
             System.out.printf("Amount of package is RM%.2f\n", total);
 
             //get array index
@@ -92,6 +103,8 @@ public class main {
             System.out.print("\nAnother booking(Y/N)?:  ");
             ans = input.next().charAt(0);
             isExist = false;
+
+            booking = new Booking(customerName, customerPackage, booking.getQuantity());
         }while(ans == 'y' || ans == 'Y');
 
         System.out.println("Summary report by agent\n");
@@ -99,6 +112,7 @@ public class main {
         System.out.println("----------------------------------");
         for(int i = 0; i < Agent.getAgentCounts(); i++)
             System.out.printf("     %d. %-20s RM%.2f\n", i+1, agent[i].getName(), agent[i].getCommission());
+
 
     }
 
